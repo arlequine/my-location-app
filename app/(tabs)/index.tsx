e95ddf690c86bc4569d14e54b2b42f7a867166b8
Arlequin <arlequine666.ah@gmail.com>
2024-05-08 00:52:58 -0600

    feat(*):add asyncStorage and get current location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,51 +1,81 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import { Image, StyleSheet, Button } from 'react-native';
+import * as Location from 'expo-location';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function HomeScreen() {
+  const [locations, setLocations] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('locations').then((value) => {
+      console.log('value', value)
+      setLocations(JSON.parse(value))
+    })
+  }, [])
+  
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    console.log('status', status)
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let { coords } = await Location.getCurrentPositionAsync({});
+    console.log('location', coords)
+    let locObj = {
+      lat: coords.latitude,
+      long: coords.longitude
+    } 
+    console.log([...locations, locObj])
+    console.log(JSON.stringify(locations))
+    setLocations([...locations, locObj]);
+    try {
+      await AsyncStorage.setItem('locations', JSON.stringify(locations));
+    } catch (e) {
+      // saving error
+    }
+
+  }
+  
+
+  // #1D3D47
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+       headerBackgroundColor={{ light: '#A1CEDC', dark: 'rgb(42 65 73 / 50%)' }}
+       headerImage={
+         <Image
+           source={require('@/assets/images/arlequin-icon.png')}
+           style={styles.reactLogo}
+         />
+       }>
+       <ThemedView style={styles.titleContainer}>
+         <ThemedText type="title">Bienvenido!</ThemedText>
+         <HelloWave />
+       </ThemedView>
+          <Button
+             title="Press me" 
+            onPress={() => getLocation()} />
+            
+       <ThemedView style={styles.titleContainer}>
+         {
+           locations.map((loc, i) => (
+            <ThemedView style={styles.coorsContainer}  key={i}>
+              <ThemedText type="default">latitude:{loc.lat}</ThemedText>
+              <ThemedText type="default">latitude:{loc.long}</ThemedText>
+            </ThemedView>
+          ))
+         }
+       </ThemedView>
+
     </ParallaxScrollView>
   );
 }
@@ -53,6 +83,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
     gap: 8,
   },
@@ -61,10 +92,17 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
+    height: '100%',
     width: 290,
     bottom: 0,
     left: 0,
     position: 'absolute',
   },
+  coorsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderBottomWidth: 2,
+    borderColor: '#ffffff'
+  }
 });
